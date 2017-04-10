bbe30667f1a13cb0cc620a608ad5806af5fbb905
yilin1991 <houyilin1991@163.com>
2017-04-10 19:18:03 +0800

    2017-04-10 19:17:57

---
+++ VanyinAdmin/Login.aspx
@@ -0,0 +1,75 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="VanyinAdmin.Login" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>领意文化管理系统平台</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body id="loginBody">
+    <form runat="server" id="myForm">
+        <div id="loginHeader">
+            <div class="loginHeaderLeft">
+                <ul>
+                    <li>
+                        <img src="/img/temp/login_small.png" /></li>
+                    <li>欢迎登录领意文化管理系统平台</li>
+                </ul>
+            </div>
+            <div class="loginHeaderRight">
+                <ul>
+                    <li><a href="#">回首页</a></li>
+                    <li><a href="#">帮助</a></li>
+                    <li><a href="#">关于</a></li>
+                </ul>
+            </div>
+        </div>
+        <div id="loginContent">
+            <div class="loginContentLogo">
+                <img src="/img/temp/loginlogo.png" />
+            </div>
+            <div class="loginContentForm">
+                <div class="loginContentFormLeft">
+                    <img src="/img/temp/Logo1.jpg" />
+                </div>
+                <div class="loginContentFormRight">
+                    <div class="loginContentForm_Title">
+                        <p><span>用户登录</span><span>UserLogin</span></p>
+                    </div>
+                    <div class="loginContentForm_Input">
+                        <div class="loginContentForm_InputImg">
+                            <img src="/img/user_ico.png" />
+                        </div>
+                        <div class="loginContentForm_InputText">
+                            <asp:TextBox runat="server" ID="txtName" CssClass="loginText" placeholder="请输入用户名"></asp:TextBox>
+                        </div>
+                    </div>
+                    <div class="loginContentForm_Input">
+                        <div class="loginContentForm_InputImg">
+                            <img src="/img/user_pass.png" />
+                        </div>
+                        <div class="loginContentForm_InputText">
+                            <asp:TextBox runat="server" ID="txtPass" CssClass="loginText" TextMode="Password" placeholder="请输入密码"></asp:TextBox>
+                        </div>
+                    </div>
+                    <div class="loginContentForm_Btn">
+                        <ul>
+                            <li>
+                                <asp:Button runat="server" ID="btnLogin" Text="登录" CssClass="btnLogin" OnClick="btnLogin_Click" />
+                            </li>
+                            <li>
+                                <asp:Label runat="server" ID="lbMessage"></asp:Label>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="loginFooter">
+            <p>版权所有 2015 上海领意文化传播有限公司</p>
+        </div>
+    </form>
+</body>
+</html>
+
---
+++ VanyinAdmin/Login.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VanyinAdmin
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        Vanyin.BLL.AdminInfo bllAdmin = new Vanyin.BLL.AdminInfo();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (Session["loginnum"] == null)
+            {
+                Session["loginnum"] = 0;
+            }
+
+            if (int.Parse(Session["loginnum"].ToString()) <= 3)
+            {
+                if (!string.IsNullOrWhiteSpace(txtName.Text) & !string.IsNullOrWhiteSpace(txtPass.Text))
+                {
+                    string name = txtName.Text;
+                    string pwd = Vanyin.Common.DESEncrypt.Encrypt(txtPass.Text.Trim());
+
+                    DataSet ds = bllAdmin.GetList("Account='" + name + "' and Pwd='" + pwd + "'");
+                    if (ds.Tables[0].Rows.Count > 0)
+                    {
+                        if (string.Equals(ds.Tables[0].Rows[0]["StateInfo"].ToString(), "1"))
+                        {
+                            Session["loginnum"] = null;
+                            Session["AdminId"] = ds.Tables[0].Rows[0]["Id"].ToString();
+                            Session["AdminAccount"] = ds.Tables[0].Rows[0]["Account"].ToString();
+
+                            Session.Timeout = 45;
+
+                            Vanyin.Common.Utils.WriteCookie("AdminAccount", Vanyin.Common.DESEncrypt.Encrypt(ds.Tables[0].Rows[0]["Account"].ToString()));
+                            Vanyin.Common.Utils.WriteCookie("AdminPwd", ds.Tables[0].Rows[0]["pwd"].ToString());
+
+                            //if (string.IsNullOrEmpty(ds.Tables[0].Rows[0]["LoginTime"].ToString()))
+                            //{
+                            //    //bllAdmin.UpdateFiled(ds.Tables[0].Rows[0]["Id"].ToString(), "EndLoginTime='" + DateTime.Now + "'");
+                            //}
+                            //else
+                            //{
+                            //    //bllAdmin.UpdateFiled(ds.Tables[0].Rows[0]["Id"].ToString(), "EndLoginTime='" + ds.Tables[0].Rows[0]["LoginTime"].ToString() + "'");
+                            //}
+                            //bllAdmin.UpdateFiled(ds.Tables[0].Rows[0]["Id"].ToString(), "LoginTime='" + DateTime.Now + "'");
+
+
+                            Response.Redirect("index.aspx");
+
+                        }
+                        else
+                        {
+                            lbMessage.Text = "您的帐号已被禁用，请联系管理人员";
+                        }
+                    }
+                    else
+                    {
+                        lbMessage.Text = "请输入正确的用户名密码";
+                        Session["loginnum"] = (int.Parse(Session["loginnum"].ToString()) + 1).ToString();
+                    }
+                }
+                else
+                {
+                    lbMessage.Text = "用户名或密码不能为空";
+                }
+            }
+            else
+            {
+                lbMessage.Text = "密码输入错误超过3次，请关闭浏览器重新登录";
+            }
+        }
+    }
+}
